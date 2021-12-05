712f1335e849754ac800351ddf94cf0eb950f9c4
Sanjeelathapa7 <sanjeelathapamagar1234@gmail.com>
2021-12-05 10:36:47 +0545

    labwork assignment

---
+++ Day2/lab2.py
@@ -0,0 +1,5 @@
+#find BMI of a person where take mass and height as input from BMI=mass in kg/(height in m/2)
+m=int(input("enter a mass in kg"))
+h=int(input("enter height in meter"))
+BMI=m/h
+print(f"{BMI} is you BMI kg/m\00b2")
---
+++ Day2/lab3.py
@@ -0,0 +1,7 @@
+#Given the integer N - the number of minutes that is passed since midnight - 
+N=int(input("enter the number of minutes passed since midnight"))
+hours=N/60
+minute=N%60
+print(f"the time passed is {hours} and {minute}minutes.")
+
+
