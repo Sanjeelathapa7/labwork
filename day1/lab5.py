c85b02efda5a532798c0f63005bb991effa7fa53
Sanjeelathapa7 <sanjeelathapamagar1234@gmail.com>
2021-12-05 10:37:15 +0545

    assignment two

--- day1/lab5.py
+++ day1/lab5.py
@@ -1,4 +1,4 @@
 #sum of first 10 odd numbers
-num=[1,3,5,7,9,11,13,15,19]
+num=[1,3,5,7,9]
 sum=sum(num)
 print(f"the sum of odd number is {sum}")
--- day1/lab6.py
+++ day1/lab6.py
@@ -1,4 +1,4 @@
 #sum of first 10 even numbers
-num=[2,4,6,8,10,12,14,16,18]
+num=[2,4,6,8,10]
 sum=sum(num)
 print(f"print the sum of even number is {sum}")
